e09c039b1bdf5d1a58b76089ea75f0b4fb506270
VictoriaCar <122562523+VictoriaCar@users.noreply.github.com>
2023-11-14 15:58:52 -0500

    fix indexing error

--- spikesortingVTJason/spikesortingVTJason.py
+++ spikesortingVTJason/spikesortingVTJason.py
@@ -77,15 +77,16 @@ import numpy as np
 deleteEv = []
 
 for i in range(len(spike_index)):
-    before = range(spike_index(i)-50, spike_index(i)-20,1)
-    after = range(spike_index(i)+20, spike_index(i)+50,1)
+    before = range(spike_index[i]-50, spike_index[i]-20,1)
+    after = range(spike_index[i]+20, spike_index[i]+50,1)
     
     #enumarate will return a tuple that contains index and value of each element 
     beforeFind = [index for index, val in enumerate(spikes[before]) if val < -10 or val >25]
     afterFind = [index for index, val in enumerate(spikes[after]) if val < -10 or val > 25]
     
     if len(beforeFind) > 1 or len(afterFind) > 1:
-        deleteEv[end + 1] = i
+        #deleteEv.append(i)
+        deleteEv[end_spike_index + 1] = i #n or end? 
 
 for i in deleteEv:
     spike_index[i] = 0
