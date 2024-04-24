b970a6280adbdd79dcfebf34895168834bed5236
Danny731 <1429709492@qq.com>
2024-04-24 17:35:48 -0400

    update

--- Code/Raster plot and Heatmap/McsMatlabDataTools/+McsHDF5/version.py
+++
@@ -1,5 +0,0 @@
-def version():
-    print("This is version 1.3.1 of the McsHDF5 Python toolbox. (08/24/2022)")
-    print("Newer versions will be available at")
-    print("\thttp://www.multichannelsystems.com/software/multi-channel-datamanager")
-    print("\thttps://github.com/multichannelsystems/McsMatlabDataTools")
